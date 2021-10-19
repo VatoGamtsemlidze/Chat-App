7f0d2644f0487ce015c4e36cbb08750e0dc95ab2
unknown <vatogamtsemlidze@gmail.com>
2021-10-19 19:05:31 +0400

    b

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -7,13 +7,16 @@ function ChatMessage({message}){
     const {text, uid, photoURL} = message;
 
     const isCurrentUser = uid === auth.currentUser.uid;
+    // const isCurrentUser = false
 
     const messageClass = isCurrentUser ? 'sent' : 'received';
     return(
         <div className={isCurrentUser ? "relative my-2 mx-3 rounded-full bg-blue-300 p-3" : "relative my-2 mx-3 rounded-full bg-gray-300 p-3"}>
-            <Image src={photoURL} alt="" className="" layout="fill" className="rounded-fulls" objectPosition="right" objectFit="contain"/>
-            <p>{text}</p>
-            <p className={isCurrentUser ? "text-xs text-blue-500" : "text-xs text-gray-500"} >{messageClass}</p>
+            <Image src={photoURL} alt="" className="" layout="fill" className="rounded-fulls" objectPosition={isCurrentUser ? "right" : "left"} objectFit="contain"/>
+            <div className={isCurrentUser ? "" : "flex flex-col items-end"}>
+                <p>{text}</p>
+                <p className={isCurrentUser ? "text-xs text-blue-500" : "text-xs text-gray-500"} >{messageClass}</p>
+            </div>
         </div>
     )
 }
